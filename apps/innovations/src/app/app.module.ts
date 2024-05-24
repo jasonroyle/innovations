aa8fdd66c3c619ea6f95d1818103d57cc15b3e69
Jason Royle <jason.royle@coxautoinc.com>
2024-05-24 12:28:36 +0100

    Enable Redux Devtools

--- apps/innovations/src/app/app.module.ts
+++ apps/innovations/src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
@@ -23,6 +25,10 @@ import { StoreRouterConnectingModule } from '@ngrx/router-store';
         },
       }
     ),
+    StoreDevtoolsModule.instrument({
+      logOnly: !isDevMode(),
+      maxAge: 25
+    }),
     EffectsModule.forRoot([]),
     StoreRouterConnectingModule.forRoot(),
   ],
