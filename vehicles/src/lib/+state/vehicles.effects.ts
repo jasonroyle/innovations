892160beafecf9451da578db5251b61db39bf032
Jason Royle <jason.royle@coxautoinc.com>
2024-06-05 14:33:05 +0100

    Only fetch vehicles on init when store is empty

--- vehicles/src/lib/+state/vehicles.effects.ts
+++ vehicles/src/lib/+state/vehicles.effects.ts
@@ -1,23 +1,34 @@
 import { Injectable, inject } from '@angular/core';
-import { Actions, OnInitEffects, createEffect, ofType } from '@ngrx/effects';
-import { Action } from '@ngrx/store';
+import {
+  Actions,
+  OnInitEffects,
+  concatLatestFrom,
+  createEffect,
+  ofType,
+} from '@ngrx/effects';
+import { Action, Store } from '@ngrx/store';
 import { switchMap, catchError, of } from 'rxjs';
 
 import { VehiclesService } from '../services/vehicles.service';
 import * as VehiclesActions from './vehicles.actions';
 import * as VehiclesFeature from './vehicles.reducer';
+import * as VehiclesSelectors from './vehicles.selectors';
 
 @Injectable()
 export class VehiclesEffects implements OnInitEffects {
   private actions$ = inject(Actions);
+  private _store: Store<VehiclesFeature.VehiclesState> = inject(Store);
   private _vehiclesService = inject(VehiclesService);
 
   init$ = createEffect(() =>
     this.actions$.pipe(
       ofType(VehiclesActions.initVehicles),
-      switchMap(() => {
-        const vehicles = this._vehiclesService.loadVehicles();
-        return of(VehiclesActions.loadVehiclesSuccess({ vehicles }))
+      concatLatestFrom(() =>
+        this._store.select(VehiclesSelectors.selectAllVehicles)
+      ),
+      switchMap(([, vehicles]) => {
+        if (!vehicles.length) vehicles = this._vehiclesService.loadVehicles();
+        return of(VehiclesActions.loadVehiclesSuccess({ vehicles }));
       }),
       catchError((error) => {
         console.error('Error', error);
