3594c58ebcd01317b9f1a3d105171d3cf2ad51ed
Jason Royle <jason.royle@coxautoinc.com>
2024-06-05 16:14:22 +0100

    Migrated to the Shared library

---
+++ libs/shared/src/lib/components/master-detail/master-detail.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cw-master-detail',
+  templateUrl: './master-detail.component.html',
+  styleUrl: './master-detail.component.scss',
+})
+export class MasterDetailComponent {}
--- libs/shared/src/lib/shared.module.ts
+++ libs/shared/src/lib/shared.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MasterDetailComponent } from './components/master-detail/master-detail.component';
+
+const exports = [MasterDetailComponent];
+
 @NgModule({
   imports: [CommonModule],
+  declarations: [...exports],
+  exports,
 })
 export class SharedModule {}
