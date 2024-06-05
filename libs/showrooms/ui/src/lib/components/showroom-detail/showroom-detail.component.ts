8a95354771571349d1b089bfc37a7098f1b2e082
Jason Royle <jason.royle@coxautoinc.com>
2024-06-05 16:29:32 +0100

    Migrated to the Showrooms UI library

---
+++ libs/showrooms/ui/src/lib/components/showroom-detail/showroom-detail.component.ts
@@ -0,0 +1,45 @@
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { VehicleDetail, VehiclesFacade } from '@codeweavers/vehicles';
+import { Observable, Subject, takeUntil } from 'rxjs';
+
+import { ShowroomsFacade } from '../../+state/showrooms.facade';
+
+@Component({
+  selector: 'cw-showroom-detail',
+  templateUrl: './showroom-detail.component.html',
+  styleUrl: './showroom-detail.component.scss',
+})
+export class ShowroomDetailComponent {
+  private readonly _destroy$ = new Subject<void>();
+  private readonly _route = inject(ActivatedRoute);
+  private readonly _router = inject(Router);
+  private readonly _showroomsFacade = inject(ShowroomsFacade);
+  private readonly _vehiclesFacade = inject(VehiclesFacade);
+  public readonly showroomDetail$ =
+    this._showroomsFacade.selectedShowroomDetail$;
+  public vehicleDetails$?: Observable<VehicleDetail[]>;
+
+  constructor() {
+    this._route.data
+      .pipe(takeUntil(this._destroy$))
+      .subscribe(({ showroom }) => {
+        if (!showroom) {
+          this._router.navigate(['../'], { relativeTo: this._route });
+          return;
+        }
+        this._showroomsFacade.dispatch(
+          this._showroomsFacade.actions.selectShowroom_showroomDetail({
+            id: showroom.id,
+          })
+        );
+        this.vehicleDetails$ =
+          this._vehiclesFacade.selectVehicleDetailsByShowroomId(showroom.id);
+      });
+  }
+
+  public ngOnDestroy(): void {
+    this._destroy$.next();
+    this._destroy$.complete();
+  }
+}
--- libs/showrooms/ui/src/lib/showrooms-ui.module.ts
+++ libs/showrooms/ui/src/lib/showrooms-ui.module.ts
@@ -1,9 +1,27 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ShowroomsApiModule } from '@codeweavers/showrooms-api';
+
+import { AddShowroomComponent } from './components/add-showroom/add-showroom.component';
+import { AddVehicleComponent } from './components/add-vehicle/add-vehicle.component';
+import { ShowroomDetailComponent } from './components/showroom-detail/showroom-detail.component';
+import { ShowroomListComponent } from './components/showroom-list/showroom-list.component';
 import { showroomsUiRoutes } from './lib.routes';
+import { ShowroomsComponent } from './pages/showrooms/showrooms.component';
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(showroomsUiRoutes)],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(showroomsUiRoutes),
+    ShowroomsApiModule,
+  ],
+  declarations: [
+    AddShowroomComponent,
+    AddVehicleComponent,
+    ShowroomDetailComponent,
+    ShowroomListComponent,
+    ShowroomsComponent,
+  ],
 })
 export class ShowroomsUiModule {}
