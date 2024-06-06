ad3323a48d9552617e22e2c71acf56fc738edcee
Jason Royle <jason.royle@coxautoinc.com>
2024-06-06 15:50:03 +0100

    Migrated Innovations app to Showroom app

--- apps/showroom/src/app/app.component.ts
+++ apps/showroom/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+interface NavItem {
+  commands: any[];
+  text: string;
+}
+
 @Component({
   selector: 'innov-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'showroom';
+  navItems: NavItem[] = [
+    {
+      commands: ['/vehicles'],
+      text: 'Vehicles',
+    },
+    {
+      commands: ['/showrooms'],
+      text: 'Showrooms',
+    },
+  ];
 }
--- apps/showroom/src/app/app.routes.ts
+++ apps/showroom/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'manufacturers',
+    loadChildren: () =>
+      import('@innovations/manufacturers-ui').then(
+        (m) => m.ManufacturersUiModule
+      ),
+  },
+  {
+    path: 'showrooms',
+    loadChildren: () =>
+      import('@innovations/showrooms-ui').then((m) => m.ShowroomsUiModule),
+  },
+  {
+    path: 'vehicles',
+    loadChildren: () =>
+      import('@innovations/vehicles-ui').then((m) => m.VehiclesUiModule),
+  },
+  { path: '', pathMatch: 'full', redirectTo: 'vehicles' },
+];
