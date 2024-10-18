3eeb25db0b9e5a0c2de57a55a10f5b8c19cba910
Jason Royle <me@jasonroyle.com>
2024-10-18 16:05:39 +0100

    Added palette, theme, color indicator

--- libs/shared/src/lib/shared.module.ts
+++ libs/shared/src/lib/shared.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
+import { ColorIndicatorComponent } from './components/color-indicator/color-indicator.component';
 import { MasterDetailComponent } from './components/master-detail/master-detail.component';
 import { SearchComponent } from './components/search/search.component';
 import { SiPrefixPipe } from './pipes/si-prefix.pipe';
 
-const exports = [MasterDetailComponent, SearchComponent, SiPrefixPipe];
+const exports = [
+  ColorIndicatorComponent,
+  MasterDetailComponent,
+  SearchComponent,
+  SiPrefixPipe,
+];
 
 @NgModule({
   imports: [CommonModule, ReactiveFormsModule, RouterModule],
--- libs/showrooms/ui/src/lib/components/vehicle-list/vehicle-list.component.ts
+++ libs/showrooms/ui/src/lib/components/vehicle-list/vehicle-list.component.ts
@@ -1,7 +1,17 @@
-import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
-import { VehiclesEntity, VehiclesFacade } from '@innovations/vehicles-api';
+import { Component, EventEmitter, Output, inject } from '@angular/core';
+import { VehiclesFacade } from '@innovations/vehicles-api';
+import { BehaviorSubject, firstValueFrom, map, tap } from 'rxjs';
 
 import { ShowroomsUiFacade } from '../../+state/showrooms-ui.facade';
+import { VehicleDetail } from '../../+state/showrooms-ui.models';
+
+interface Vehicle {
+  hovered: boolean;
+  selected: boolean;
+  vehicleDetail: VehicleDetail;
+}
+
+type Vehicles = Record<string, Vehicle>;
 
 @Component({
   selector: 'innov-vehicle-list',
@@ -10,47 +20,96 @@ import { ShowroomsUiFacade } from '../../+state/showrooms-ui.facade';
 })
 export class VehicleListComponent {
   private readonly _showroomsUiFacade = inject(ShowroomsUiFacade);
-  private _vehicleSelectionEnabled = false;
+  private readonly _vehicles$ = new BehaviorSubject<Vehicles>({});
   private readonly _vehiclesFacade = inject(VehiclesFacade);
-  @Output() public readonly vehicleSelect = new EventEmitter<VehiclesEntity>();
-  public selectedVehicles: VehiclesEntity[] = [];
   public readonly showroomDetail$ =
-    this._showroomsUiFacade.selectedShowroomDetail$;
-  public readonly vehicleDetails$ =
-    this._showroomsUiFacade.selectedShowroomVehicleDetails$;
+    this._showroomsUiFacade.selectedShowroomDetail$.pipe(
+      tap((showroomDetail) =>
+        this._vehicles$.next(
+          (showroomDetail?.vehicleDetails ?? []).reduce<Vehicles>(
+            (vehicles, vehicleDetail) => ({
+              ...vehicles,
+              [vehicleDetail.vehicle.registrationNumber]: {
+                hovered: false,
+                selected: false,
+                vehicleDetail,
+              },
+            }),
+            {}
+          )
+        )
+      )
+    );
+  @Output() public readonly vehicleSelect = new EventEmitter<VehicleDetail>();
+  public vehicleSelectionEnabled = false;
+  public readonly vehicles$ = this._vehicles$.asObservable();
+  public readonly selectedVehicles$ = this.vehicles$.pipe(
+    map((vehicles) =>
+      Object.keys(vehicles)
+        .filter((registrationNumber) => vehicles[registrationNumber].selected)
+        .map((registrationNumber) => vehicles[registrationNumber].vehicleDetail)
+    )
+  );
 
-  public get vehicleSelectionEnabled(): boolean {
-    return this._vehicleSelectionEnabled;
-  }
-  public set vehicleSelectionEnabled(enabled: boolean) {
-    this._vehicleSelectionEnabled = enabled;
-    if (!enabled) this.selectedVehicles = [];
+  public get selectedVehicles(): Promise<VehicleDetail[]> {
+    return firstValueFrom(this.selectedVehicles$);
   }
 
-  public removeVehicles(): void {
-    this.selectedVehicles.forEach((vehicle) =>
+  public get vehicles(): Promise<Vehicles> {
+    return firstValueFrom(this._vehicles$);
+  }
+
+  private async _getVehicle(
+    registrationNumber: string
+  ): Promise<[Vehicle, Vehicles]> {
+    const vehicles = await this.vehicles;
+    const vehicle = vehicles[registrationNumber];
+    if (!vehicle) throw 'Invalid vehicle registration number';
+    return [vehicle, vehicles];
+  }
+
+  private async _patchVehicle(
+    registrationNumber: string,
+    patch: Partial<Vehicle>
+  ): Promise<void> {
+    const [vehicle, vehicles] = await this._getVehicle(registrationNumber);
+    this._vehicles$.next({
+      ...vehicles,
+      [registrationNumber]: {
+        ...vehicle,
+        ...patch,
+      },
+    });
+  }
+
+  public async hoverVehicle(vehicle: Vehicle, hovered: boolean): Promise<void> {
+    this._patchVehicle(vehicle.vehicleDetail.vehicle.registrationNumber, {
+      hovered,
+    });
+  }
+
+  public async removeVehicles(): Promise<void> {
+    const selectedVehicles = await this.selectedVehicles;
+    selectedVehicles.forEach(({ vehicle: { registrationNumber } }) =>
       this._vehiclesFacade.dispatch(
         this._vehiclesFacade.actions.linkShowroom_showroomVehicleList({
-          registrationNumber: vehicle.registrationNumber,
+          registrationNumber,
         })
       )
     );
-    this.selectedVehicles = [];
     this.vehicleSelectionEnabled = false;
   }
 
-  public selectVehicle(vehicle: VehiclesEntity): void {
+  public async selectVehicle(
+    vehicle: Vehicle,
+    selected?: boolean
+  ): Promise<void> {
     if (this.vehicleSelectionEnabled) {
-      const index = this.selectedVehicles.findIndex(
-        ({ registrationNumber }) =>
-          registrationNumber === vehicle.registrationNumber
-      );
-      const selectedVehicles = [...this.selectedVehicles];
-      if (index === -1) selectedVehicles.push(vehicle);
-      else selectedVehicles.splice(index, 1);
-      this.selectedVehicles = [...selectedVehicles];
+      this._patchVehicle(vehicle.vehicleDetail.vehicle.registrationNumber, {
+        selected: selected ?? !vehicle.selected,
+      });
     } else {
-      this.vehicleSelect.emit(vehicle);
+      this.vehicleSelect.emit(vehicle.vehicleDetail);
     }
   }
 
